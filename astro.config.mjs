7f0ce36fd055554477cd1e4ff0fbc0d55f9bf974
miral_kashiwagi <miral@infoseek.jp>
2025-02-28 19:49:18 +0900

    ベースパスを/motionに変更

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/motion',
   integrations: [
     tailwind({
       // Use the CommonJS configuration file
